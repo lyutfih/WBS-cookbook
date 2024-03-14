be035a010b4fa73ad41c392f5b6f57c0c902fdf8
Lyutfi <lutfickl@hotmail.com>
2024-03-14 19:37:40 +0100

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'indigo-1000': '#382C52',
+      },
+    },
+  },
+  plugins: [],
+}
+
